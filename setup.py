5fd0cc61931cef2fa34880f0a9b47339e6df1985
Max Countryman <maxc@me.com>
2013-04-29 07:01:01 -0700

    updating Requests

--- setup.py
+++ setup.py
@@ -10,9 +10,8 @@ if sys.argv[-1] == 'test':
     status >>= 8
     sys.exit(status)
 
-install_requires = ['requests==1.1.0']
-if os.environ.get('TRAVIS') == 'true' and \
-    os.environ.get('TRAVIS_PYTHON_VERSION').startswith('2.6'):
+install_requires = ['requests==1.2.0']
+if sys.version_info[0] == 2 and sys.version_info[1] < 7:
     install_requires.append('unittest2>=0.5.1')
 
 setup(name='rauth',
