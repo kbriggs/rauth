1d560d2d56c9cd269dcb30ee640c13cc899e5564
Marcus Whybrow <marcus@marcuswhybrow.net>
2013-05-14 13:39:48 +0100

    Prevent double encoding of oauth_token in get_authorize_url
    urllib.urlencode automatically applies urllib.quote_plus to all keys
    and values, so applying urllib.quote before hand will result in double
    encoding of the token.
    
    This double encoding doesn't cause a problem, of course, if the token
    consists of regular characters. But an equals sign (=), for example,
    would get encoded once to "%3D", and then again to "%253D".

--- rauth/service.py
+++ rauth/service.py
@@ -9,8 +9,7 @@
 from rauth.session import OAuth1Session, OAuth2Session, OflySession
 from rauth.utils import ENTITY_METHODS, parse_utf8_qsl
 
-from rauth.compat import quote, urlencode
-
+from urllib import urlencode
 
 PROCESS_TOKEN_ERROR = ('Decoder failed to handle {key} with data as returned '
                        'by provider. A different decoder may be needed. '
@@ -21,7 +20,7 @@ def process_token_request(r, decoder, *args):
     try:
         data = decoder(r.content)
         return tuple(data[key] for key in args)
-    except KeyError as e:  # pragma: no cover
+    except KeyError, e:  # pragma: no cover
         bad_key = e.args[0]
         raise KeyError(PROCESS_TOKEN_ERROR.format(key=bad_key, raw=r.content))
 
@@ -226,7 +225,7 @@ class OAuth1Service(Service):
             request querystring.
         :type \*\*params: dict
         '''
-        params.update({'oauth_token': quote(request_token)})
+        params.update({'oauth_token': request_token})
         return self.authorize_url + '?' + urlencode(params)
 
     def get_raw_access_token(self,
